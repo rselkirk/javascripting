8079ef17e7f492d0de3e52d35c7ac5fed896fac7
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-03-15 18:15:32 +0000

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,11 @@
+var total;
+var total = 0;
+
+var limit;
+var limit = 10;
+
+for (var i = 0; i < limit; i++)
+{
+  total += i;
+}
+console.log(total);
---
+++ rounding-numbers.js
@@ -0,0 +1,5 @@
+var roundUp;
+var roundUp = 1.5;
+var rounded;
+var rounded = Math.round(roundUp);
+console.log(rounded);
